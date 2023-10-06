65600417c9d77ada06360cf8cbba098223b9c480
bitterlox <bitterlox@pm.me>
2023-10-06 20:20:24 +0000

    add config for maker

--- flake.nix
+++ flake.nix
@@ -118,6 +118,63 @@
           }
         ];
       };
+      "maker" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # The Nix module system can modularize configuration,
+        # improving the maintainability of configuration.
+        #
+        # Each parameter in the `modules` is a Nix Module, and
+        # there is a partial introduction to it in the nixpkgs manual:
+        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
+        # It is said to be partial because the documentation is not
+        # complete, only some simple introductions.
+        # such is the current state of Nix documentation...
+        #
+        # A Nix Module can be an attribute set, or a function that
+        # returns an attribute set. By default, if a Nix Module is a
+        # function, this function can only have the following parameters:
+        #
+        #  lib:     the nixpkgs function library, which provides many
+        #             useful functions for operating Nix expressions:
+        #             https://nixos.org/manual/nixpkgs/stable/#id-1.4
+        #  config:  all config options of the current flake, every useful
+        #  options: all options defined in all NixOS Modules
+        #             in the current flake
+        #  pkgs:   a collection of all packages defined in nixpkgs,
+        #            plus a set of functions related to packaging.
+        #            you can assume its default value is
+        #            `nixpkgs.legacyPackages."${system}"` for now.
+        #            can be customed by `nixpkgs.pkgs` option
+        #  modulesPath: the default path of nixpkgs's modules folder,
+        #               used to import some extra modules from nixpkgs.
+        #               this parameter is rarely used,
+        #               you can ignore it for now.
+        #
+        # Only these parameters can be passed by default.
+        # If you need to pass other parameters,
+        # you must use `specialArgs` by uncomment the following line:
+        #
+        # specialArgs = {...}  # pass custom arguments into all sub module.
+        modules = [
+          # Import the configuration.nix here, so that the
+          # old configuration file can still take effect.
+          # Note: configuration.nix itself is also a Nix Module,
+          ./machines/maker/configuration.nix
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # TODO replace ryan with your own username
+            home-manager.users.angel = import ./machines/maker/users/angel.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      }
     };
   };
 }
