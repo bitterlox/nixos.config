dd78aa0715a79bd37653572c541e42bfea8e7d5c
bitterlox <bitterlox@pm.me>
2023-10-27 13:17:14 +0200

    extract secrets glue code into separate file

--- common/default.nix
+++ common/default.nix
@@ -1,35 +1,4 @@
 { pkgs }: {
-  build-machine-secrets = secrets-flake: machine:
-    let
-      secrets = {
-        chani = {
-          ssh-private-key = {
-            file = secrets-flake.chani.ssh.private-key;
-            mode = "600";
-            owner = "angel";
-            group = "users";
-          };
-          password = {
-            file = secrets-flake.chani.password;
-            mode = "600";
-            owner = "angel";
-            group = "users";
-          };
-        };
-        sietch = {
-          ssh-private-key = {
-            file = secrets-flake.sietch.ssh.private-key;
-            mode = "600";
-            owner = "angel";
-            group = "users";
-          };
-          password = {
-            file = secrets-flake.sietch.password;
-            mode = "600";
-            owner = "angel";
-            group = "users";
-          };
-        };
-      };
-    in (builtins.getAttr machine secrets);
+  build-machine-secrets = secrets-flake: machine-name:
+    import (./secrets.nix) { inherit secrets-flake machine-name; };
 }
