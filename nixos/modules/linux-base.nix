f4a3eac94c997c002e8907794e22d348921a5211
bitterlox <bitterlox@pm.me>
2024-01-24 12:23:11 +0100

    enable mosh server;

--- nixos/modules/linux-base.nix
+++ nixos/modules/linux-base.nix
@@ -33,6 +33,11 @@
     '';
   };
 
+  programs.mosh = {
+    enable = true;
+#    openFirewall = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
