795d5ccf4740330aadb0355544b763b8a66c348b
bitterlox <bitterlox@pm.me>
2024-05-09 17:01:53 +0200

    wip;

--- nixos/configurations/sietch/default.nix
+++ nixos/configurations/sietch/default.nix
@@ -1,31 +1,30 @@
 # this is akin to a flake-parts top-level module
 { inputs, config, lib, getSystem, moduleWithSystem, privateModules, withSystem
 , ... }:
-let
-  nixosModules = config.flake.nixosModules;
-  nixosBaseModules = with nixosModules; [ agenix linux-base ];
+let shared-modules = config.flake.nixosModules;
 in {
   flake.nixosConfigurations = {
     "sietch" = withSystem "x86_64-linux" (ctx@{ inputs', system, pkgs, ... }:
       inputs.nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [{ nixpkgs.pkgs = pkgs; }] ++ privateModules
-          ++ nixosBaseModules ++ [
-            ./modules/soft-serve
-            (import ./configuration.nix config.flake.lib)
-            # make home-manager as a module of nixos
-            # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
+        modules = [{ nixpkgs.pkgs = pkgs; }] ++ privateModules ++ [
+          shared-modules.agenix
+          shared-modules.linux-base
+          ./modules/soft-serve
+          (import ./configuration.nix config.flake.lib)
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-              # TODO replace ryan with your own username
-              home-manager.users.angel = import config.flake.homeModules.angel;
+            # TODO replace ryan with your own username
+            home-manager.users.angel = import config.flake.homeModules.angel;
 
-              # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-            }
-          ];
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
       });
   };
 }
---
+++ nixos/configurations/sietch/modules/secrets.nix
@@ -0,0 +1,39 @@
+secrets-flake: agenix-module:
+{ config, options, pkgs, lib, ... }: {
+  imports = [ agenix-module ];
+  config = {
+    age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    age.secrets = {
+      ssh-private-key = {
+        file = secrets-flake.sietch.ssh.private-key;
+        mode = "600";
+        owner = "angel";
+        group = "users";
+      };
+      borg-passphrase = {
+        file = secrets-flake.sietch.borg-passphrase;
+        mode = "600";
+        owner = "angel";
+        group = "users";
+      };
+      password = {
+        file = secrets-flake.sietch.password;
+        mode = "600";
+        owner = "angel";
+        group = "users";
+      };
+    };
+    # WIP fill in stuff in lockbox and add the cleartext stuff alongside
+    # not sure if we need to split out the filling of the lockbox to load
+    # the agenix module first to decrypt everything
+    lockbox = { };
+  };
+  options = {
+    lockbox = lib.mkOption {
+      type = with lib.types; attrsOf str;
+      default = { };
+      example = { mysecret = "superSecretPassword"; };
+      description = lib.mdDoc "An attrset of secrets and less secret values";
+    };
+  };
+}
