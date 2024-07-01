581fcbd3fe330be382e328932545b644ce98baa5
bitterlox <bitterlox@pm.me>
2024-07-01 17:29:07 +0200

    data-importer package builds correcty;

--- packages/firefly-iii-data-importer.nix
+++ packages/firefly-iii-data-importer.nix
@@ -1,4 +1,5 @@
-{ lib, fetchFromGitHub, php83 }:
+{ lib, fetchFromGitHub, buildNpmPackage, php83
+, dataDir ? "/var/lib/firefly-iii-data-importer" }:
 
 let
   pname = "firefly-iii-data-importer";
@@ -13,6 +14,22 @@ let
     hash = "sha256-xGYaSoK6luGTZ2/waGbnnvdXk1IoyseSbD/uW8KIqto=";
   };
 
+  assets = buildNpmPackage {
+    pname = "${pname}-assets";
+    inherit version src;
+    npmDepsHash = "sha256-ZSHMDalFM3Iu7gL0SoZ0akrS5UAH1FOWTRsGjzM7DWA=";
+    dontNpmBuild = true;
+    # from nixpkgs docs:
+    # ... even if you don’t use them directly, it is good practice to do so anyways for
+    # downstream users who would want to add a postInstall by overriding your derivation.
+    installPhase = ''
+      runHook preInstall
+      npm run build --workspace=v2
+      cp -r ./public $out/
+      runHook postInstall
+    '';
+  };
+
 in phpPackage.buildComposerProject (finalAttrs: {
   inherit pname src version;
 
@@ -23,19 +40,17 @@ in phpPackage.buildComposerProject (finalAttrs: {
   #    tests = nixosTests.firefly-iii;
   #  };
 
-  #  postInstall = ''
-  #    mv $out/share/php/${pname}/* $out/
-  #    rm -R $out/share $out/storage $out/bootstrap/cache $out/public
-  #    ln -s ${dataDir}/storage $out/storage
-  #    ln -s ${dataDir}/cache $out/bootstrap/cache
-  #  '';
+  postInstall = ''
+    mv $out/share/php/${pname}/* $out/
+    rm -R $out/share $out/storage $out/bootstrap/cache $out/public
+    cp -a ${assets} $out/public
+    ln -s ${dataDir}/storage $out/storage
+    ln -s ${dataDir}/cache $out/bootstrap/cache
+  '';
 
   meta = {
     changelog =
-      "https://github.com/firefly-iii/firefly-iii/releases/tag/v${version}";
-    description = "Firefly III: a personal finances manager";
-    homepage = "https://github.com/firefly-iii/firefly-iii";
-    license = lib.licenses.agpl3Only;
-    maintainers = [ lib.maintainers.savyajha ];
+      "https://github.com/firefly-iii/data-importer/releases/tag/v${version}";
+    description = "Data importer for Firefly III";
   };
 })
