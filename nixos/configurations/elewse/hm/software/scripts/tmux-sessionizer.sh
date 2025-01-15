7473a6cdd95b0390e6c7e99ac7fa6d4fb442f8de
bitterlox <bitterlox@pm.me>
2025-01-15 19:07:01 +0100

    fix tmux-sessionizer;

--- nixos/configurations/elewse/hm/software/scripts/tmux-sessionizer.sh
+++ nixos/configurations/elewse/hm/software/scripts/tmux-sessionizer.sh
@@ -29,4 +29,8 @@ if ! tmux has-session -t="$selected" name 2>/dev/null; then
   tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-tmux switch-client -t "$selected_name"
+if [[ -z $TMUX ]]; then
+  tmux attach-session -t "$selected_name"
+else
+  tmux switch-client -t "$selected_name"
+fi
