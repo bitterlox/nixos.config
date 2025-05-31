4cb6f7595d7a8d87bdd10f92b6d5eeedd0d8f9a6
angel <angel@MacBookPro.homenet.telecomitalia.it>
2025-05-31 16:57:21 +0200

    bump stable inputs;

--- flake.nix
+++ flake.nix
@@ -19,12 +19,12 @@
   inputs = {
     ## nixpkgs ##
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     ## home-manager ##
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager-stable.url = "github:nix-community/home-manager/release-24.11";
+    home-manager-stable.url = "github:nix-community/home-manager/release-25.05";
     home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     ## secrets ##
